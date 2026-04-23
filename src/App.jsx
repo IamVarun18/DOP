6c3c65df1d904966d3030399d08ff35c12950def
IamVarun18 <varun.018072001@gmail.com>
2026-04-24 02:11:15 +0530

    fix import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Work from "./components/Work";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Showreel from "./components/Showreel";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Showreel />
       <Work />
       <About />
-       <Contact />
+      <Contact />
       <Footer />
     </>
   );
