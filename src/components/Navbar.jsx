d15622f55bb31dbf1d27ef66da4e746d20b00cd2
IamVarun18 <varun.018072001@gmail.com>
2026-04-24 02:19:39 +0530

    fix all CSS casing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-import "./Navbar.css";
+import "./navbar.css";
 
 const navLinks = [
   { label: "Work", href: "#work" },
